acb37b5dcdd2d935234bb7b248e70c1b4b5588d9
santiago2424241-coder <santiago2424241@gmail.com>
2026-04-10 11:53:45 -0500

    Update transporte.py

--- transporte.py
+++ transporte.py
@@ -30,7 +30,7 @@ except:
 
 
 # ==================== CONFIGURACIÓN SUPABASE ====================
-SUPABASE_DB_URL = ""
+SUPABASE_DB_URL = "postgresql://postgres.wiomyjrmsrhcgvhgkbqe:Conejito800$@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
 
 
 # ==================== FUNCIONES DE FORMATO ====================
